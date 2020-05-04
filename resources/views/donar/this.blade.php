fa4c602bcbcf3262386a135f1daf35932bf74554
esor-poudel <esor.poudel@gmail.com>
2020-05-04 17:00:37 +0545

    bladefile

---
+++ resources/views/donar/this.blade.php
@@ -0,0 +1,12 @@
+<div class="form-group">
+    <label for="name">Last Donate Date</label>
+    <input type="date" name="date" class="form-control">
+    </div>
+
+    
+    <div class="form-group">
+        <div class="text-center">
+        <button class="btn btn-success " type="submit">update profile</button>
+     </div>
+     @else
+     <h1>please fill the Donar Form</h1>
